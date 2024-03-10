75e86d5a15905347a56ec0b0cdc7e8da7114fa81
muhammadalive <muhammadali17abc@gmail.com>
2024-03-10 19:14:04 +0500

    hotfix btn schema

--- app/bot/services/external/aiogram/shortcut/schema.py
+++ app/bot/services/external/aiogram/shortcut/schema.py
@@ -3,15 +3,16 @@ schema shortcuts
 """
 
 
-def get_schema(items):
+def get_schema(num_buttons):
     """
     get the schema of the items
     """
-    if items == 1:
+    if num_buttons == 1:
         return [1]
 
-    if items // 2 == 0:
-        return [2, 2]
+    if num_buttons == 2:
+        return [1, 1]
 
-    if items // 2 == 1:
-        return [2, 1]
+    if num_buttons > 2:
+        num_rows = (num_buttons - 1) // 2 + 1
+        return [2] * (num_rows - 1) + [num_buttons % 2 or 2]
