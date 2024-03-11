e1d4a7d4bd0aeb0511c087cb26ae0a43e47a197e
muhammadalive <muhammadali17abc@gmail.com>
2024-03-11 05:47:59 +0500

    small changes

--- app/bot/controllers/controller.py
+++ app/bot/controllers/controller.py
@@ -1,10 +1,15 @@
 """
 implementation of controller interface
 """
+import logging
+
 from app.bot import models
 from app.bot import repositories
 
 from app.bot.controllers import IAppController
+
+
+logger = logging.getLogger(__name__)
 
 
 class AppController(IAppController):
@@ -21,7 +26,13 @@ class AppController(IAppController):
         self,
         client: models.Client
     ) -> models.Client | None:
-        await self.repository.create_or_update_client(client)
+        try:
+            await self.repository.create_or_update_client(client)
+
+        # pylint: disable=W0718
+        except Exception as exc:
+            logger.error("create_or_update_client - %s", exc)
+            return
 
     async def get_categories(self) -> list[models.Category]:
         return await self.repository.get_categories()
